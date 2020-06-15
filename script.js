db325aedc29345797c9cc85030567ae5d06a8abe
junghole <dchoi315@gmail.com>
2020-06-15 19:40:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
+
+$('#work-myregistry-title').click(()=>{
+    $("#work-myregistry-desc").slideToggle();
+});
+
+
+$('#work-prestige-title').click(()=>{   
+    $("#work-prestige-desc").slideToggle();
+});
+
+$('#work-tutor-title').click(()=>{   
+    $("#work-tutor-desc").slideToggle();
+});
+
